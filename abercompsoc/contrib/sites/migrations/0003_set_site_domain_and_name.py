0be69b8ec7486b4c8ae21d68a665fd66439e1f53
vCra <aaw13@aber.ac.uk>
2018-12-10 22:34:59 +0000

    Add Redis Cache

--- abercompsoc/contrib/sites/migrations/0003_set_site_domain_and_name.py
+++ abercompsoc/contrib/sites/migrations/0003_set_site_domain_and_name.py
@@ -13,7 +13,7 @@ def update_site_forward(apps, schema_editor):
     Site.objects.update_or_create(
         id=settings.SITE_ID,
         defaults={
-            "domain": "abercompsoc.com",
+            "domain": "abercs.herokuapp.com",
             "name": "AberCompSoc",
         },
     )
--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,14 @@ from .base import env
 # ------------------------------------------------------------------------------
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': env('REDIS_URL'),
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+            # Mimicing memcache behavior.
+            # http://niwinz.github.io/django-redis/latest/#_memcached_exceptions_behavior
+            'IGNORE_EXCEPTIONS': True,
+        }
     }
 }
 
